c64870683bbd36d25d7231bacc0ca4149911157f
Ragnvindr08 <johnstevenimperialcaspe08@gmail.com>
2025-12-03 22:30:53 +0800

    Add build.sh for Renders

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
 
-# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py migrate --settings=core.deployment_settings
-
-# Collect static files
-python manage.py collectstatic --noinput --settings=core.deployment_settings
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
